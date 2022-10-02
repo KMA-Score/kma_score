55b0f0a363282660516840eb9082ca2e01277630
arahiko-ayami <zinang7@gmail.com>
2022-10-02 18:08:32 +0700

    add: seo optimization

--- src/views/StudentPage/index.jsx
+++ src/views/StudentPage/index.jsx
@@ -8,6 +8,7 @@ import { useParams } from "react-router-dom";
 import Loading from "../../components/Loading";
 import NotFound from "../NotFound";
 import Alert from "../../components/Alert";
+import { Helmet } from "react-helmet";
 
 export default function StudentPage() {
   const [studentStatistics, setStudentStatistics] = useState(null);
@@ -67,46 +68,63 @@ export default function StudentPage() {
   if (!studentStatistics) return <NotFound />;
 
   return (
-    <div className="md:flex overflow-auto bg-black px-5 md:px-24 lg:px-36 pt-16 md:pt-28 pb-5">
-      <div className="space-y-2 w-full">
-        <h2>{studentStatistics?.name}</h2>
-        <section className="flex space-x-2 overflow-auto">
-          <Chip>
-            <ion-icon
-              name="person-circle-outline"
-              class="text-xl pr-2"
-            ></ion-icon>
-            {studentStatistics?.id}
-          </Chip>
-          <Chip>
-            <ion-icon name="golf-outline" class="text-xl pr-2"></ion-icon>
-            {studentStatistics?.class}
-          </Chip>
-        </section>
-        <section className="flex flex-col lg:flex-row w-full space-y-2 lg:space-y-0 lg:space-x-5 justify-between w-full">
-          <Box cssClass={COLORS.GREEN + " w-full"}>
-            Số môn đã hoàn thành
-            <h3>{studentStatistics?.passedSubjects}</h3>
-          </Box>
-          <Box cssClass={COLORS.RED + " w-full"}>
-            Số môn còn nợ
-            <h3>{studentStatistics?.failedSubjects}</h3>
-          </Box>
-          <Box cssClass={COLORS.BLUE + " w-full"}>
-            GPA
-            <h3>{studentStatistics?.avgScore}</h3>
-          </Box>
-        </section>
-        <Alert type="warning">
-          Do điểm các môn tiếng Anh cơ bản có thể quy đổi từ các chứng chỉ, điểm
-          được hiển thị có thể không chính xác
-        </Alert>
-        <section className="overflow-x-auto">
-          {studentStatistics && (
-            <Table data={studentStatistics?.scores} columns={tableHeader} />
-          )}
-        </section>
+    <>
+      <Helmet>
+        <title>{`./kma_score --name=${studentStatistics.name}`}</title>
+        <meta
+          property="og:title"
+          content={`./kma_score --name=${studentStatistics.name}`}
+        />
+        <meta
+          name="description"
+          content={`Điểm số của ${studentStatistics.name}`}
+        />
+        <meta
+          name="og:description"
+          content={`Điểm số của ${studentStatistics.name}`}
+        />
+      </Helmet>
+      <div className="md:flex overflow-auto bg-black px-5 md:px-24 lg:px-36 pt-16 md:pt-28 pb-5">
+        <div className="space-y-2 w-full">
+          <h2>{studentStatistics?.name}</h2>
+          <section className="flex space-x-2 overflow-auto">
+            <Chip>
+              <ion-icon
+                name="person-circle-outline"
+                class="text-xl pr-2"
+              ></ion-icon>
+              {studentStatistics?.id}
+            </Chip>
+            <Chip>
+              <ion-icon name="golf-outline" class="text-xl pr-2"></ion-icon>
+              {studentStatistics?.class}
+            </Chip>
+          </section>
+          <section className="flex flex-col lg:flex-row w-full space-y-2 lg:space-y-0 lg:space-x-5 justify-between w-full">
+            <Box cssClass={COLORS.GREEN + " w-full"}>
+              Số môn đã hoàn thành
+              <h3>{studentStatistics?.passedSubjects}</h3>
+            </Box>
+            <Box cssClass={COLORS.RED + " w-full"}>
+              Số môn còn nợ
+              <h3>{studentStatistics?.failedSubjects}</h3>
+            </Box>
+            <Box cssClass={COLORS.BLUE + " w-full"}>
+              GPA
+              <h3>{studentStatistics?.avgScore}</h3>
+            </Box>
+          </section>
+          <Alert type="warning">
+            Do điểm các môn tiếng Anh cơ bản có thể quy đổi từ các chứng chỉ,
+            điểm được hiển thị có thể không chính xác
+          </Alert>
+          <section className="overflow-x-auto">
+            {studentStatistics && (
+              <Table data={studentStatistics?.scores} columns={tableHeader} />
+            )}
+          </section>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
