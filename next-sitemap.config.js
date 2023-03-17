e152c9bdb3001f6386d5f61af6003730f44aa44e
arahiko-ayami <zinang7@gmail.com>
2023-03-17 10:31:01 +0700

    fix: next-sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-  siteUrl: "https://score.superkma.com",
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL ?? "https://score.superkma.com",
   generateRobotsTxt: true,
   generateIndexSitemap: false,
+  exclude: ["/api/*", "/server-sitemap.xml"],
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      `${
+        process.env.NEXT_PUBLIC_SITE_URL ?? "https://score.superkma.com"
+      }/server-sitemap.xml`,
+    ],
+  },
 };
