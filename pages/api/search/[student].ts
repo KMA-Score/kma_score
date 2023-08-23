efc6ce8b223d831e88c01bf74954c0149dedd1b1
Dang Hoang Phuc <dhp371.work@gmail.com>
2023-08-24 00:18:27 +0700

    chore: pump version nextjs + dependency & add prettier config

--- pages/api/search/[student].ts
+++ pages/api/search/[student].ts
@@ -3,7 +3,7 @@ import { api } from "../../../utils/axios";
 
 export default async function searchStudentByIdHandler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse,
 ) {
   const { student } = req.query;
   const { data } = await api.get(`/search?query=${student}`);
